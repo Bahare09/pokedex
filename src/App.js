3580399cbbe477517c3577a1c127f98f6fe160fa
Bahare <itxbb09@gmail.com>
2023-03-07 11:39:15 +0000

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Logo from "./Logo";
 import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
+//import PokemonMoves from "./PokemonMoves";
+import PokemonMovesSelector from "./PokemonMovesSelector";
+import PokemonCity from "./PokemonCity";
 
 function App() {
   const logWhenClicked = () => {
@@ -12,6 +15,8 @@ function App() {
       <Logo appName="Pokedex" handleClick={logWhenClicked} />
       <BestPokemon abilities={["Anticipation", "Adaptability", "Run-Away"]} />
       <CaughtPokemon date={new Date().toLocaleDateString()} />
+      <PokemonMovesSelector />
+      <PokemonCity />
     </div>
   );
 }
--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -2,12 +2,27 @@ import React, { useState } from "react";
 
 const CaughtPokemon = (props) => {
   const [caught, setCaught] = useState([]);
-  const catchPokemon = () => setCaught(caught.concat("first"));
+  const [pokemonNameInput, setPokemonNameInput] = useState("");
+  const catchPokemon = () => {
+    if (pokemonNameInput !== "") {
+      //lesson 3 exe-G-11
+      setCaught(caught.concat(pokemonNameInput.trim())); // trim for words like "  bb" that has space before chars
+      setPokemonNameInput(""); //for cleaning the input after click
+    }
+  };
+  const handleInputChange = (event) => {
+    setPokemonNameInput(event.target.value);
+  };
   return (
     <div>
       <p>
         Caught {caught.length} Pokemon on {props.date}
       </p>
+      <input
+        type="text"
+        value={pokemonNameInput}
+        onChange={handleInputChange}
+      />
       <button onClick={catchPokemon}>click</button>
       <ul>
         {caught.map((item, index) => {
